0a3b08505cd61a7df5f19cc3235b03c10275453b
SucharitaMullapudi <60675281+SucharitaMullapudi@users.noreply.github.com>
2020-02-29 23:57:54 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 var express = require('express');
 var cors = require('cors');
+var axios = require('axios');
 var app = express();
 app.use(cors());
 
 const port = 3000
 
-app.get('/', (req, res) => {
-    
-    res.send('Hello World!')
-    //res.sendFile('index.html', {root : __dirname})
-})
+app.get('/', function(req, res) => {
+    var data =axios.get(" https://pnyv9hc928.execute-api.us-east-2.amazonaws.com/prod")
+ .then((response) =>{
+    res.send=(response.data);
+             });
+
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
